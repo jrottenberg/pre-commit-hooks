c013ae5e47d45448b17c05e9f5842160fb5c9c64
Julien Rottenberg <julien@rottenberg.info>
2021-06-13 22:24:57 -0700

    fix: doc

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="pre-commit-terraform",
-    description="Pre-commit hooks for frigate",
+    description="Pre-commit hooks for frigate and other awesome utilities",
     url="https://github.com",
     version_format="{tag}+{gitsha}",
     author="Contributors",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
