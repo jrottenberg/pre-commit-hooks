a21e207ef4685fd9c06b1f78cdbce9d478da6424
Julien Rottenberg <julien.rottenberg@disney.com>
2022-08-29 10:02:32 -0700

    pinned_update
    gitlab first

---
+++ pre_commit_hooks/pinned_update/gitlab_ci.py
@@ -0,0 +1,49 @@
+# gitlab_ci functionnality
+
+
+from urllib.parse import urlparse
+
+import ruamel.yaml
+
+from pre_commit_hooks.utils import cmd_output, get_all_tags, highest_version
+
+
+yaml = ruamel.yaml.YAML()
+
+yaml.indent(mapping=2, sequence=4, offset=2)
+
+
+def update_gitlab_ci(gitlab_ci: str) -> int:
+    retval = 0
+
+    with open(gitlab_ci, "r") as gitlab_ci_file:
+        original_gitlab_ci_object = yaml.load(
+            gitlab_ci_file,
+        )
+    updated_gitlab_ci_object = original_gitlab_ci_object
+
+    if "include" not in original_gitlab_ci_object:
+        return retval
+
+    _, gitlab_origin, _ = cmd_output("git", "remote", "get-url", "origin")
+    if gitlab_origin.startswith("http"):
+        url = urlparse(gitlab_origin)
+        prefix = f"{url.scheme}://{url.netloc}/"
+    else:
+        prefix = gitlab_origin.split(":")[0] + ":"
+
+    for elt in updated_gitlab_ci_object['include']:
+        if "ref" in elt:
+            position =  updated_gitlab_ci_object['include'].index(elt)
+            all_tags = get_all_tags(prefix + elt["project"])
+            latest = highest_version(all_tags)
+            if elt["ref"] != latest:
+                elt["ref"] = latest
+                updated_gitlab_ci_object['include'][position] = elt
+                retval += 1
+
+    if retval > 0:
+        with open(gitlab_ci, "w") as gitlab_ci_file:
+            yaml.dump(updated_gitlab_ci_object, gitlab_ci_file)
+
+    return retval
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="pre-commit-terraform",
-    description="Pre-commit hooks for frigate and other awesome utilities",
-    url="https://github.com",
-    version_format="{tag}+{gitsha}",
-    author="Contributors",
+    name="pre-commit-hooks",
+    description="Pre-commit hooks",
+    url="https://github.com/jrottenberg/pre-commit-hooks",
+    version_format="{tag}",
+    author="Julien Rottenberg",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     packages=find_packages(exclude=("tests*", "testing*")),
-    install_requires=[
-        "frigate",
-    ],
+    install_requires=["ruamel.yaml", "packaging"],
     entry_points={
         "console_scripts": [
-            "pre_commit_frigate = pre_commit_hooks.frigate:main",
+            "pinned_update = pre_commit_hooks.pinned_update:main",
         ],
     },
 )
