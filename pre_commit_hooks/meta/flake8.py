4872f67e0c62f717957164156d98be824e454f92
Julien Rottenberg <julien@rottenberg.info>
2022-11-19 14:50:28 -0800

    a regexp

--- pre_commit_hooks/meta/flake8.py
+++ pre_commit_hooks/meta/flake8.py
@@ -5,6 +5,6 @@ import re
 
 
 def adjust_flake8_url(pre_commit_config: str) -> int:
-    flake8_regexp = re.compile("gitlab.com/pycqa/flake8", re.IGNORECASE)
-    return re.sub(flake8_regexp, pre_commit_config)
+    flake8_regexp = re.compile(r"gitlab.com\/pycqa\/flake8", re.IGNORECASE)
+    return re.sub(flake8_regexp, "github.com/pycqa/flake8", pre_commit_config)
 
