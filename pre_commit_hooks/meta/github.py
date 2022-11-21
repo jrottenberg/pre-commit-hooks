a71ff0953cf6e27ac8e77e84624b9c3179496dd4
Julien Rottenberg <julien@rottenberg.info>
2022-11-20 18:33:52 -0800

    align

--- pre_commit_hooks/meta/github.py
+++ pre_commit_hooks/meta/github.py
@@ -10,7 +10,6 @@ def replace_github_protocol(pre_commit_config: ruamel.yaml.comments.CommentedMap
     i = 0
     for item in pre_commit_config['repos']:
         replacement = re.sub(github_regexp, "https://github.com", item['repo'])
-        pre_commit_config['repos'][i].repo = replacement
-        i += 1
+        pre_commit_config['repos'][i]['repo'] = replacement
     return pre_commit_config
 
